f87f943e124230c30bf182bb10e6fe43a6475900
BRUFHALO <ronaldo.somostinn@gmail.com>
2026-02-03 16:32:56 -0400

    correccion de cords

--- Documents/UNEXCA 2025/proyectoFinal/backend/main.py
+++ Documents/UNEXCA 2025/proyectoFinal/backend/main.py
@@ -39,7 +39,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Frontend URLs
+    allow_origins=["*"],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
